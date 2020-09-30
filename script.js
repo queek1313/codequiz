31035cd81de25499cc0a3001d6189d13e5f07ba3
tony nolan <tonynolan313@gmail.com>
2020-09-30 09:36:53 -0600

    writtin code

---
+++ script.js
@@ -0,0 +1,117 @@
+// Need to create variables for holding score of quiz
+// need Variables .getElementById for html 
+// 
+// need to create questions for quiz in Array with objects for question and answers
+
+//use bootstrap to style the page
+//each question as an object with  answers to access.
+//  need to create a function that runs for each question with 3 or 4 buttons.
+// 
+// need to set a timer for quiz in JS
+// 
+
+//When a question shows up on screen there needs to be a mutibile choice of answers
+// a wrong answer on the quiz will take time from the clock
+// when all questions are answered or the timer reaches 0 the game is over
+// show score and let user save initials 
+
+var quizContainer = document.getElementById('quiz-container');
+var resultsContainer = document.getElementById('results');
+var startQuiz = document.getElementById("startQuiz")
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var quizTimer = document.getElementById("quizTimer");
+var answerButton = document.getElementsByClassName("answerButton");
+
+var myQuestions = [
+    {
+        question: "DOM stands for ____",
+        answers: ["A: Document Object Model ", " B: Document Operation Method ", " C: Download Object Model "],
+        correctAnswer: "a"
+    },
+    {
+        question: "Functions are code that _____",
+        answers: ["A: Hide Code ","B: Runs code over and over ", "C: creates a loop of code "],
+        correctAnswer: "b"
+    },
+    {
+        question: "HTML stands for _____",
+        answers: ["A: Hyper Threat Management Level ","B: Happy Text Making Language ","C: Hyper Text Markup Language"],
+        correctAnswer: "c"
+    }
+];
+var finalQuestionIndex = myQuestions.length;
+var currentQuestionIndex = 0;
+var score = 0;
+var timeLeft = 45;
+var timerInterval;
+var i = 0;
+
+startQuiz.addEventListener("click", start);
+
+function start() {
+    nextQ();
+    startQuiz.style.display = "none";
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        // console.log(timeLeft)
+        quizTimer.textContent = "Time Left: " + timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            console.log("time up")
+        }
+    }, 1000)
+}
+
+function nextQ() {
+    document.getElementById("answer").innerHTML = "";
+    var q = " Question: " + myQuestions[i].question;
+    document.getElementById("q").innerHTML = q;
+    for (var j = 0; j < myQuestions[i].answers.length; j++) {
+        var a = myQuestions[i].answers[j];
+        document.getElementById("answer").append(a);
+    } 
+    
+    
+}
+function testAnswer(val) {
+    console.log(val)
+    if (val === myQuestions[i].correctAnswer){
+        alert("correct")
+        i++;
+        score++;
+        document.getElementById("answer").innerHTML = resultsContainer;
+        checkScore();
+
+    }else{
+        alert("wrong")
+        i++;
+        timeLeft - 5;
+       checkScore();
+    }
+}
+function checkScore(){
+    if (i==3){
+        endGame();
+    } else {
+        nextQ()
+    }
+}
+  function endGame(){ 
+    document.getElementById("answer").innerHTML = "";
+    document.getElementById("q").innerHTML = "";
+    alert("game over you scored " + score)
+  } 
+    
+
+
+    // create a function for the timer
+
+
+
+
+//create a function for that displays the end score
+
+
+//create a function that ends the game and lets user play again
